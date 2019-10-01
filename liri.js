2cd8219d40f5e111b739247aaa4fda673c5ac77e
Scottie <avscott@pacbell.net>
2019-09-30 22:17:28 -0700

    add npm packages

--- liri.js
+++ liri.js
@@ -1,16 +1,29 @@
 //add npm packages
+//DotEnv
+require("dotenv").config();
+
+//Axios
+let axios = reqiure("axios");
+
+//spotify
+let Spotify = require("node-spotify-api");
+
+let spotify = new Spotify({
+    id: "<your spotify client id>",
+    secret: "<your spotify client secret>"
+});
+
+//moment
+let moment = require("moment");
+moment().format();
+
 //To retrieve the data that will power this app, you'll need to send requests using the axios package to the Bands in Town, Spotify and OMDB APIs. You'll find these Node packages crucial for your assignment.
 
 
 
 // Node-Spotify-API
 
-// Axios
+
 
 
 // You'll use Axios to grab data from the OMDB API and the Bands In Town API
-
-
-
-// Moment
-// DotEnv
