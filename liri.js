fd053d3d954d4bbb8d4343df2c4d4cbb14c3648d
Scottie <avscott@pacbell.net>
2019-09-30 18:42:04 -0700

    initial commit

---
+++ liri.js
@@ -0,0 +1,16 @@
+//add npm packages
+//To retrieve the data that will power this app, you'll need to send requests using the axios package to the Bands in Town, Spotify and OMDB APIs. You'll find these Node packages crucial for your assignment.
+
+
+
+// Node-Spotify-API
+
+// Axios
+
+
+// You'll use Axios to grab data from the OMDB API and the Bands In Town API
+
+
+
+// Moment
+// DotEnv
