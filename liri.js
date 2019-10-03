8821c797422c06213fde078c67252ec57d5c746e
Scottie <avscott@pacbell.net>
2019-10-02 18:33:21 -0700

    concert-this function

--- liri.js
+++ liri.js
@@ -3,13 +3,13 @@
 require("dotenv").config();
 
 //connect to keys.js
-let keys = require("./keys.js");
+const keys = require("./keys.js");
 
 //Axios
-let axios = reqiure("axios");
+const axios = require("axios");
 
 //fs
-let fs = require("fs");
+const fs = require("fs");
 
 //spotify
 // let Spotify = require("node-spotify-api");
@@ -21,7 +21,7 @@ let fs = require("fs");
 
 //moment
 let moment = require("moment");
-moment().format();
+// moment().format();
 
 //To retrieve the data that will power this app, you'll need to send requests using the axios package to the Bands in Town, Spotify and OMDB APIs. You'll find these Node packages crucial for your assignment.
 
@@ -29,8 +29,25 @@ moment().format();
 
 // Node-Spotify-API
 
+// let getArtist = async (artist) => {
+//     let response = await axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+//     console.log(response.data)
+// }
+
 
 
 
 // You'll use Axios to grab data from the OMDB API and the Bands In Town API
+let artist = process.argv[2];
+console.log(artist)
 
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+.then(function(response) {
+    console.log(response.data);
+})
+// concert-this
+
+
+// spotify-this-song
+// movie-this
+// do-what-it-says
